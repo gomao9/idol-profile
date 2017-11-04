33f8a1330d32332a167960101c47b451984dccf8
gomao9 <gomaoq@gmail.com>
2017-11-05 02:13:21 +0900

    add profiles

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ class Idol {
       this[key.slice(1)] = value;
     };
 
-    this.weightInt = parseInt(this.weight, 10);
-    this.heightInt = parseInt(this.height, 10);
-    this.ageInt    = parseInt(this.age, 10);
+    this.weightInt   = parseInt(this.weight, 10);
+    this.heightInt   = parseInt(this.height, 10);
+    this.ageInt      = parseInt(this.age, 10);
+    this.shoeSizeInt = parseInt(this.shoeSize, 10);
     this.bmi = this.weightInt / ((this.heightInt /100.0) ** 2);
+
   }
 
   bmiStr() { return isNaN(this.bmi) ? '' : this.bmi.toFixed(2) }
@@ -34,8 +36,28 @@ class Idol {
     return isNaN(this.weightInt) ? this.weight : `${this.weightInt}kg`;
   }
 
-  ageStr() {
-    return this.age ? `${this.age}歳` : '';
+  ageStr() { return this.age ? `${this.age}歳` : ''; }
+
+   shoeSizeStr() { return isNaN(this.shoeSizeInt) ? '' : `${this.shoeSizeInt}kg` }
+
+  handednessStr() {
+    switch (this.handedness) {
+      case 'right':
+        return '右手';
+      case 'left':
+        return '左手';
+      case 'both':
+        return '両手';
+      default:
+        return '';
+    }
+  }
+
+  threeSize() {
+    if (!this.bust) {
+      return '';
+    }
+    return `${this.bust}-${this.waist}-${this.hip}`
   }
 }
 
